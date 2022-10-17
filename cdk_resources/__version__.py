6e8441f63676cdcb0aa7be5fe65ff9162e7bcb8d
Sergio Fernandez <sfernandezf90@gmail.com>
2022-10-17 10:30:59 -0400

    [INTEGRATION][2.0.0]
    * Bumping Version for 2.0.0

--- cdk_resources/__version__.py
+++ cdk_resources/__version__.py
@@ -7,7 +7,7 @@
 __name__ = "cdk_resources"
 __description__ = "Resources based CDK."
 __url__ = "https://.readthedocs.io"
-__version__ = "1.0.0"
+__version__ = "2.0.0"
 __author__ = "Sergio Fernandez"
 __license__ = "MIT License"
 __copyright__ = "Copyright 2021 Sergio Fernandez"
